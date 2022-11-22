27b1baa8b04bd6e9a8cd7b71308f8a6de7850ff8
Khanh Duy Nguyen <55223995+duynguyen2001@users.noreply.github.com>
2022-11-22 17:01:52 -0600

    add layout

--- cs424-project3/src/App.js
+++ cs424-project3/src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Routes, Route, HashRouter } from "react-router-dom";
-import { ThemeProvider } from 'theme-ui'
+import { ThemeProvider } from "theme-ui";
 import theme from "./theme";
 import BrushMode from "./pages/BrushMode";
 import SliderMode from "./pages/SliderMode";
@@ -11,27 +11,22 @@ import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 
 export default function App() {
-  return (
-    <div>
-
-<ThemeProvider theme={theme}>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="BrushMode" element={ <BrushMode/>} />
-          <Route path="SelectMode" element={ <SelectMode/>} />
-          <Route path="SliderMode" element={ <SliderMode/>} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-      </HashRouter>
-      </ThemeProvider>
-    </div>
-  );
+    return (
+        <div>
+            <ThemeProvider theme={theme}>
+                <HashRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<Home />} />
+                            <Route path="about" element={<About />} />
+                            <Route path="BrushMode" element={<BrushMode />} />
+                            <Route path="SelectMode" element={<SelectMode />} />
+                            <Route path="SliderMode" element={<SliderMode />} />
+                            <Route path="*" element={<NoMatch />} />
+                        </Route>
+                    </Routes>
+                </HashRouter>
+            </ThemeProvider>
+        </div>
+    );
 }
