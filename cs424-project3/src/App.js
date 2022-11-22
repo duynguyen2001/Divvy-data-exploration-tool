4e13bf64293f46e85a15bf0400901a76e48bff31
Khanh Duy Nguyen <55223995+duynguyen2001@users.noreply.github.com>
2022-11-22 15:46:06 -0600

    add

--- cs424-project3/src/App.js
+++ cs424-project3/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { ThemeProvider } from "theme-ui";
+import theme from "./theme";
+import React, { Component } from "react";
+import { createBrowserHistory } from "history";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <ThemeProvider theme={theme}>
+            {/* <Home></Home> */}
+            <BrowserRouter basename="/Project3CS424">
+                <Routes>
+                    <Route index component={<Home/>} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ cs424-project3/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+const Home = () => {
+    return<div><h1>Hello World</h1></div>
+}
+
+
+export default Home;
